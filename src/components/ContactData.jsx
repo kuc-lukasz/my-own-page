f041b6aed26a817e6bf3780a2e08a6fa28639ebc
Luka Ku <lukaszkuc93@gmail.com>
2022-02-08 11:51:44 +0100

    add information to data about projects

--- src/components/ContactData.jsx
+++ src/components/ContactData.jsx
@@ -8,20 +8,20 @@ import LinkedInIcon from "../icons/linkedin.png";
 export const ContactData = () => {
     return (
         <ul className={CD.mainContainer}>
-            <li className={CD.li}>
+            
                 <a href="tel:607154416" className={CD.a}>
                     <img src={CallmeIcon} alt="callMeIcon" />
                     +48 607 154 416
                 </a>
                 {/* <p>+48 607 154 416</p> */}
-            </li>
-            <li className={CD.li}>
+            
+            
                 <a href="mailto:lukaszkuc93@gmail.com" className={CD.a}>
                     <img src={TextMeIcon} alt="WriteMeIcon" />{" "}
                     lukaszkuc93@gmail.com
                 </a>
-            </li>
-            <li className={CD.li}>
+            
+            
                 <a
                     href="https://github.com/kuc-lukasz"
                     className={CD.a}
@@ -30,8 +30,8 @@ export const ContactData = () => {
                 >
                     <img src={GithubIcon} alt="GithubIcon" /> Github
                 </a>
-            </li>
-            <li className={CD.li}>
+            
+            
                 <a
                     href="https://www.linkedin.com/in/%C5%82ukasz-kuc-b83666a7/"
                     className={CD.a}
@@ -40,7 +40,7 @@ export const ContactData = () => {
                 >
                     <img src={LinkedInIcon} alt="LinkedInIcon" /> Linkedin
                 </a>
-            </li>
+           
         </ul>
     );
 };
